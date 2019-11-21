bbf742696665c644c3fc2ae323cc2fb1528edce3
Aniket Rao <aniketrao21@gmail.com>
2019-11-21 16:25:14 +0530

    Add Homework #1 - Variables
    - I picked out a random track from Beatport
    - Identified and stored track attributes as variables
    - Print Variables

---
+++ homework-1/main.py
@@ -0,0 +1,41 @@
+# This stores the track name
+track_name = "POUR THE MILK EXTENDED MIX"
+
+# This stores the beatport url
+beatport_url = "https://www.beatport.com/track/pour-the-milk-extended-mix/12706653"
+
+# This stores the beatport price
+price = 2.49
+
+# This stores the artists name
+artists = "Robbie Doherty, Keees"
+
+# This stores the track length
+length = 5.34
+
+# This stores the track release date
+release_date = "2019-11-08"
+
+# This stores the track BPM
+bpm = 127
+
+# This stores the track key
+key = "A maj"
+
+# This stores the track genre
+genre = "Tech House"
+
+# This stores the track label
+label = "Cr2 Records"
+
+
+print(track_name)
+print(beatport_url)
+print(price)
+print(artists)
+print(length)
+print(release_date)
+print(bpm)
+print(key)
+print(genre)
+print(label)
