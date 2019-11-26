d51b5131b79a0a760c828cc1626ef6e82e6d61ac
Aniket Rao <aniketrao21@gmail.com>
2019-11-26 23:15:30 +0530

    Homework - 2 - Used functions to return values - Added a function which returns if the track is a solo effort or not

---
+++ homework-2/main.py
@@ -0,0 +1,21 @@
+def genre():
+  return "Tech House"
+
+def artist():
+  return "Robbie Doherty, Keees"
+
+def releaseDate():
+  return "2019-11-08"
+
+def soloArtist():
+  return False
+
+genre = genre()
+artist = artist()
+release_date = releaseDate()
+solo_artist = soloArtist()
+
+print(genre)
+print(artist)
+print(release_date)
+print(solo_artist)
